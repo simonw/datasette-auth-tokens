934b25d53c405dedff34a49fb06369c5a1f4f429
Simon Willison <swillison@gmail.com>
2020-06-11 16:32:03 -0700

    Added tests

--- tests/test_auth_tokens.py
+++ tests/test_auth_tokens.py
@@ -1,114 +1,42 @@
 from datasette.app import Datasette
 import httpx
-import sqlite_utils
 import pytest
-
-
-def create_tables(conn):
-    db = sqlite_utils.Database(conn)
-    db["table_access"].insert_all(
-        [
-            {"user_id": 1, "database": "test", "table": "dogs"},
-            {"user_id": 2, "database": "test", "table": "dogs"},
-            {"user_id": 1, "database": "test", "table": "cats"},
-        ]
-    )
-    db["cats"].insert({"name": "Casper"})
-    db["dogs"].insert({"name": "Cleo"})
-    db["other"].insert({"name": "Other"})
-
-    # user_id = 3 is banned from 'sqlite_master'
-    db["banned"].insert({"table": "other", "user_id": 3})
 
 
 @pytest.fixture
 async def ds(tmpdir):
-    filepath = tmpdir / "test.db"
-    ds = Datasette(
-        [filepath],
+    return Datasette(
+        [],
+        memory=True,
         metadata={
             "plugins": {
-                "datasette-permissions-sql": [
-                    {
-                        "action": "view-query",
-                        "fallback": True,
-                        "resource": ["test", "sqlite_master"],
-                        "sql": """
-                            SELECT
-                                -1
-                            FROM
-                                banned
-                            WHERE
-                                user_id = :actor_id
-                        """,
-                    },
-                    {
-                        "action": "view-table",
-                        "sql": """
-                            SELECT
-                                *
-                            FROM
-                                table_access
-                            WHERE
-                                user_id = :actor_id
-                                AND "database" = :resource_1
-                                AND "table" = :resource_2
-                        """,
-                    },
+                "datasette-auth-tokens": [
+                    {"token": "one", "actor": {"id": "one"}},
+                    {"token": "two", "actor": {"id": "two"}},
                 ]
             },
-            "databases": {
-                "test": {
-                    "allow_sql": {},
-                    "queries": {"sqlite_master": "select * from sqlite_master"},
-                }
-            },
+            "databases": {":memory:": {"allow_sql": {"id": "one"},}},
         },
     )
-    await ds.get_database().execute_write_fn(create_tables, block=True)
     return ds
 
 
-@pytest.mark.asyncio
-async def test_ds_fixture(ds):
-    assert {"table_access", "cats", "dogs", "banned", "other"} == set(
-        await ds.get_database().table_names()
-    )
-
-
 @pytest.mark.parametrize(
-    "actor,table,expected_status",
+    "token,path,expected_status",
     [
-        (None, "dogs", 403),
-        (None, "cats", 403),
-        ({"id": 1}, "dogs", 200),
-        ({"id": 2}, "dogs", 200),
-        ({"id": 1}, "cats", 200),
-        ({"id": 2}, "cats", 403),
+        ("", "/", 200),
+        ("", "/:memory:?sql=select+1", 403),
+        ("one", "/", 200),
+        ("one", "/:memory:?sql=select+1", 200),
+        ("two", "/", 200),
+        ("two", "/:memory:?sql=select+1", 403),
     ],
 )
 @pytest.mark.asyncio
-async def test_permissions_sql(ds, actor, table, expected_status):
+async def test_fallback(ds, token, path, expected_status):
     async with httpx.AsyncClient(app=ds.app()) as client:
-        cookies = {}
-        if actor:
-            cookies = {"ds_actor": ds.sign({"a": actor}, "actor")}
         response = await client.get(
-            "http://localhost/test/{}".format(table), cookies=cookies
-        )
-        assert expected_status == response.status_code
-
-
-@pytest.mark.parametrize(
-    "actor,expected_status", [(None, 200), ({"id": 1}, 200), ({"id": 3}, 403),],
-)
-@pytest.mark.asyncio
-async def test_fallback(ds, actor, expected_status):
-    async with httpx.AsyncClient(app=ds.app()) as client:
-        cookies = {}
-        if actor:
-            cookies = {"ds_actor": ds.sign({"a": actor}, "actor")}
-        response = await client.get(
-            "http://localhost/test/sqlite_master", cookies=cookies
+            "http://localhost{}".format(path),
+            headers={"Authorization": "Bearer {}".format(token)},
         )
         assert expected_status == response.status_code
