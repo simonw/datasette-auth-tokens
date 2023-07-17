10b55dc66c70a5fb79ef25ee78392ad8219eec1d
Simon Willison <swillison@gmail.com>
2023-07-17 12:00:43 -0700

    Tests for live/revoked/expired tokens

--- tests/test_managed_tokens.py
+++ tests/test_managed_tokens.py
@@ -2,6 +2,7 @@ from datasette.app import Datasette
 import pytest
 import pytest_asyncio
 import sqlite_utils
+import time
 
 
 @pytest_asyncio.fixture
@@ -20,6 +21,53 @@ async def ds_managed(tmp_path_factory):
             },
         },
     )
+
+
+@pytest.mark.parametrize("status", ("live", "revoked", "expired"))
+@pytest.mark.asyncio
+async def test_live_revoked_expired_tokens(ds_managed, status):
+    token_id, token = await _create_token(ds_managed)
+    expected_actor = {"id": "root", "token": "dsatok"}
+    if status in ("revoked", "expired"):
+        expected_actor = None
+    if status == "revoked":
+        await ds_managed.get_database().execute_write(
+            "update _datasette_auth_tokens set token_status = 'R' where id=:id",
+            {"id": token_id},
+        )
+    elif status == "expired":
+        # Expire it by setting the created_timestamp and expires_after_seconds
+        await ds_managed.get_database().execute_write(
+            "update _datasette_auth_tokens set created_timestamp = :created, expires_after_seconds = 60 where id=:id",
+            {"id": token_id, "created": time.time() - 120},
+        )
+    actor_response = await ds_managed.client.get(
+        "/-/actor.json", headers={"Authorization": "Bearer {}".format(token)}
+    )
+    assert actor_response.status_code == 200
+    assert actor_response.json() == {"actor": expected_actor}
+
+
+async def _create_token(ds_managed):
+    root_cookie = ds_managed.sign({"a": {"id": "root"}}, "actor")
+    create_page = await ds_managed.client.get(
+        "/-/api/tokens/create", cookies={"ds_actor": root_cookie}
+    )
+    ds_csrftoken = create_page.cookies["ds_csrftoken"]
+    post_fields = {}
+    post_fields["csrftoken"] = ds_csrftoken
+    response = await ds_managed.client.post(
+        "/-/api/tokens/create",
+        data=post_fields,
+        cookies={"ds_actor": root_cookie, "ds_csrftoken": ds_csrftoken},
+    )
+    assert response.status_code == 200
+    api_token = response.text.split('class="copyable" style="width: 40%" value="')[
+        1
+    ].split('"')[0]
+    # Decode token to find token ID
+    token_id = ds_managed.unsign(api_token.split("dsatok_")[1], namespace="dsatok")
+    return token_id, api_token
 
 
 @pytest.mark.parametrize(
@@ -41,7 +89,6 @@ async def test_create_token(ds_managed, post_fields, expected_actor):
         "/-/api/tokens/create", cookies={"ds_actor": cookie}
     )
     assert create_page.status_code == 200
-    print(create_page.text)
     # Extract ds_csrftoken
     ds_csrftoken = create_page.cookies["ds_csrftoken"]
     # Use that to create the token
